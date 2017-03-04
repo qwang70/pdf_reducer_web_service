ee018ddee6cae5d8705b49676c45d6e42d943ea5
Jinren Ping <Jinren Ping>
2017-03-04 14:53:24 +0800

    Add media static file support

--- pdf_reducer/pdf_reducer/urls.py
+++ pdf_reducer/pdf_reducer/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('pdf_reducer_web_app.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
